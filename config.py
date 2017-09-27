113962ec50fa1697e209325ca0ec750a67a711a6
James Parks <arcsecond@gmail.com>
2017-09-26 22:01:16 -0700

    added config.py and forms.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import datetime
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'thisisatest'
+TIME_LIMIT = datetime.timedelta(minutes=20)
---
+++ forms.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, URL
+
+from wtforms.ext.csrf.session import SessionSecureForm
+import datetime
+
+class MyBaseForm(SessionSecureForm):
+    SECRET_KEY = 'thisisatest'
+    TIME_LIMIT = datetime.timedelta(minutes=20)
+
+class EmailPasswordForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+class UrlForm(Form):
+    url = StringField('URL', validators=[DataRequired(), URL()])
