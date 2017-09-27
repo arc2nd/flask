8fe111ff00a37398cbecd98b94546aaa810a0a3c
James Parks <arcsecond@gmail.com>
2017-09-26 22:00:19 -0700

    added simple (insecure) forms

--- rest.py
+++ rest.py
@@ -6,12 +6,23 @@ import json
 import pymongo
 import datetime
 
+##Call Examples
+#curl http://localhost:5002/sugar -X POST --data date=09232017 --data time=21.39 --data value=117
+#curl http://localhost:5002/chart/08232017-09232017 -X GET
+
+#import requests
+#resp = requests.get('http://localhost:5002/sugar', data={'date':'09232017', 'time':21.39, 'value':117})
+#resp = requests.get('http://localhost:5002/chart/08232017-09232017')
+
+
+
 ##make db connection
 
 ##make flask app
 app = Flask(__name__)
 api = Api(app)
 
+##setup parser and accepted arguments
 parser = reqparse.RequestParser()
 parser.add_argument('start_date')
 parser.add_argument('end_data')
@@ -31,8 +42,16 @@ class SVG(Resource):
         print('{} :: {}'.format(start, end))
         ##generate SVG text
         return {'start':start, 'end':end}
+    def post(self):
+        return 'SVG.post'
+    def put(self):
+        return 'SVG.put'
+    def delete(self):
+        return 'SVG.delete'
 
 class BloodSugar(Resource):
+    def get(self):
+        return 'BloodSugar.get'
     def post(self):
         args = parser.parse_args()
         date = args['date']
@@ -42,8 +61,14 @@ class BloodSugar(Resource):
         ret_str = 'Sugar\ndate: {}\ntime: {}\nval: {}'.format(date, time, value)
         ret_dict = {'date': date, 'time': time, 'value':value}
         return ret_dict
+    def put(self):
+        return 'BloodSugar.put'
+    def delete(self):
+        return 'BloodSugar.delete'
 
 class BloodPressure(Resource):
+    def get(self):
+        return 'BloodPressure.get'
     def post(self):
         args = parser.parse_args()
         date = args['date']
@@ -52,8 +77,14 @@ class BloodPressure(Resource):
         ##put info into mongodb
         ret_str = 'Pressure\ndate: {}\nhigh: {}\nlow: {}'.format(date, high, low)
         return ret_str
+    def put(self):
+        return 'BloodPressure.put'
+    def delete(self):
+        return 'BloodPressure.delete'
 
 class Weight(Resource):
+    def get(self):
+        return 'Weight.get'
     def post(self):
         args = parser.parse_args()
         date = args['date']
@@ -61,6 +92,10 @@ class Weight(Resource):
         ##put info into mondodb
         ret_str = 'Weight\ndate: {}\nval: {}'.format(date, value)
         return ret_str
+    def put(self):
+        return 'Weight.put'
+    def delete(self):
+        return 'Weight.delete'
 
 ##add resources to api
 api.add_resource(SVG, '/chart/<date>')
--- routes.py
+++ routes.py
@@ -3,60 +3,120 @@
 #09/20/17
 
 import os
-from flask import Flask, render_template, Response, redirect, url_for
+import forms
+from functools import wraps
+from flask import Flask, g, render_template, Response, redirect, url_for
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 root = os.getcwd()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-@app.route('/login')
-def login():
-    form = EmailPasswordForm()
-    if form.validate_on_submit():
-        ##check psswd and log in
-        return redirect(url_for('/index'))
-    return render_template('login.html')
+class User(object):
+    def __init__(self, name=None):
+        self.name=name
+
+current_user = User()
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.name is None:
+            return redirect(url_for('login')) #, next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html')
+
+@app.route('/')
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = forms.EmailPasswordForm()
+    if form.validate_on_submit():
+        if form.email.data in ['james@here.com']:
+            if form.password.data in ['thisisatest']:
+                current_user.name = form.email.data
+                return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    current_user.name = None
+    return render_template('logout.html')
 
 @app.route('/hello')
 @app.route('/hello/<name>')
 def hello(name=None):
     if not name:
+        title = os.environ['USER'].title()
         user = {'username': os.environ['USER'].title()}
     else:
+        title = name.title()
         user = {'username': name.title()}
-    return render_template('hello.html', title="{}'s Landing Page".format(name.title()), user=user
+    return render_template('hello.html', title="{}'s Landing Page".format(title), user=user)
 
-@app.route('/movies/<root>')
-def movies(root):
-    dir_contents = os.listdir(root)
-    dirs = {}
+@app.route('/movies', methods=['GET'])
+@app.route('/movies/<root>', methods=['GET'])
+@login_required
+def movies(root=''):
+    working_dir = os.path.join(os.path.expanduser('~'), root)
+    dir_contents = os.listdir(working_dir)
+    dir_contents.sort()
+    dirs = []
     for e in dir_contents:
-        if os.path.isdir(os.path.join(root, e)):
-            dirs[e] = 'dir'
-        elif os.path.isfile(os.path.join(root, e)):
-            size = 5
-            dirs[e] = size
-        elif os.path.islink(os.path.join(root, e)):
-            dirs[e] = 'link'
+        print e
+        working_path = os.path.join(working_dir, e)
+        if os.path.isdir(working_path):
+            dirs.append({'name':e, 'type':'dir', 'path':working_path})
+        elif os.path.isfile(os.path.join(working_dir, e)):
+            size = str((os.path.getsize(working_path) / 1024) / 1024) 
+            dirs.append({'name':e, 'type':'file', 'size':size, 'path':working_path})
+        elif os.path.islink(working_path):
+            dirs.append({'name':e, 'type':'link'})
+    print(dirs)
     return render_template('movies.html', title='Movies', root=root, dirs=dirs)
 
+@app.route('/download', methods=['GET', 'POST'])
+@login_required
+def download():
+    ##take in a url and run youtube-dl on it
+    form = forms.UrlForm()
+    if form.validate_on_submit():
+        cmd = 'youtube-dl {}'.format(form.url.data)
+        return redirect(url_for('submit_success'))
+    return render_template('download.html', form=form)
+
+@app.route('/convert', methods=['GET', 'POST'])
+@login_required
+def convert():
+    ##take in a url and run convert2audio on it
+    form = forms.UrlForm()
+    if form.validate_on_submit():
+        cmd = 'convert2audio -u {}'.format(form.url.data)
+        return redirect(url_for('submit_success'))
+    return render_template('convert.html', form=form)
+
+@app.route('/submit_success', methods=['GET'])
+def submit_success():
+    return render_template('submit_success.html')
+
 @app.route('/a')
+@login_required
 def a():
     return render_template('a.html', title='A\'s Landing Page')
 @app.route('/j')
+@login_required
 def j():
     return render_template('j.html', title='J\'s Landing Page')
 @app.route('/e')
+@login_required
 def e():
     return render_template('e.html', title='E\'s Landing Page')
 @app.route('/f')
+@login_required
 def f():
     return render_template('f.html', title='F\'s Landing Page')
 
